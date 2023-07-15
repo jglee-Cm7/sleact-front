1c328bda261554b690ee6aed83414d01965a29c8
jglee <jinkyu0826@gmail.com>
2023-07-15 11:21:39 +0900

    feat: Login, SignUp code splitting

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,14 +1,16 @@
-import Login from "@pages/LogIn";
-import SignUp from "@pages/SignUp";
+import loadable from "@loadable/component";
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+
+const Login = loadable(() => import("@pages/LogIn"));
+const SignUp = loadable(() => import("@pages/SignUp"));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" Component={Login} />
+      <Route path="/signup" Component={SignUp} />
     </Routes>
   );
 }
