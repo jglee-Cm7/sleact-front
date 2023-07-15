8b78fcc5dd82e5bb1a289b966a905cb110cb12a2
jglee <jinkyu0826@gmail.com>
2023-07-15 11:19:15 +0900

    feat: Login, SignUp Routing

--- index.tsx
+++ index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@layouts/App";
+import { BrowserRouter } from "react-router-dom";
 
 const appElement = document.querySelector("#app");
 if (!appElement) throw new Error("Failed to find the app element");
-ReactDOM.createRoot(appElement).render(<App />);
+ReactDOM.createRoot(appElement).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,7 +1,16 @@
+import Login from "@pages/LogIn";
+import SignUp from "@pages/SignUp";
 import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div>초기 설정</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
 }
 
 export default App;
