8ead1f564aac5293ff196a47f5018285acde5464
jglee <jinkyu0826@gmail.com>
2023-07-17 01:38:32 +0900

    feat: react-query 적용

--- index.tsx
+++ index.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@layouts/App";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const queryClient = new QueryClient();
 const appElement = document.querySelector("#app");
 if (!appElement) throw new Error("Failed to find the app element");
+
 ReactDOM.createRoot(appElement).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom" />
+    </QueryClientProvider>
   </BrowserRouter>,
 );
